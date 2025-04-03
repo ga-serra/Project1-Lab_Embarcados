26a071d963cd11659a132a0001d29f0c1b58ff74
Leo <114858492+LeoKingsCoast@users.noreply.github.com>
2025-04-02 21:36:20 -0300

    Made blink

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-# Oiééé
-# Faça seu código
+from machine import Pin
+import time
+
+red_led = Pin(12, Pin.OUT)
+
+while True:
+    red_led.value(1)
+    time.sleep(1)
+    red_led.value(0)
+    time.sleep(1)
